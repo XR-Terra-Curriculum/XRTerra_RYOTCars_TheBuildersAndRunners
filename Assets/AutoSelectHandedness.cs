e8820ccc59081cfbb508cd691e921ffbb2e5bfe5
LegioTitanicus <alexbernstein7170@gmail.com>
2020-11-07 16:05:40 -0500

    Toggle between select and teleport based on input
    Script will now automatically change which type of ray interactor is active based on 2d axis input

---
+++ Assets/AutoSelectHandedness.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class AutoSelectHandedness : MonoBehaviour
+{
+
+    public GameObject RightSelect;
+    public GameObject LeftSelect;
+    public GameObject RightTeleport;
+    public GameObject LeftTeleport;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchType(RightSelect, RightTeleport);
+        SwitchType(LeftSelect, LeftTeleport);
+    }
+
+    void SwitchType(GameObject selector, GameObject teleporter)
+    {
+        XRController controller = selector.GetComponent<XRController>();
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 vector2))
+        {
+            if (vector2.magnitude >= 0.2f)
+            {
+                print("Magnitude cutoff surpassed");
+                selector.SetActive(false);
+                teleporter.SetActive(true);
+            }
+            else
+            {
+                selector.SetActive(true);
+                teleporter.SetActive(false);
+            }
+        }
+    }
+}
