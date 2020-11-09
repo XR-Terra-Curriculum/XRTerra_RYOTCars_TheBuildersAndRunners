539abf111425c0a2f86da617c935f816826d1285
Liz Newton-Tanzer <54453455+lizecaint@users.noreply.github.com>
2020-11-09 15:23:49 -0500

    Both doors
    TBD: the shifting position of the grabbable component

---
+++ Assets/LizExportedAssets/VRUtility.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRUtility : MonoBehaviour
+{
+    public static VRUtility Instance;
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public AudioSource PlaySpatialClipAt(AudioClip clip, Vector3 pos, float volume, float spatialBlend = 1f, float randomizePitch = 0)
+    {
+
+        if (clip == null)
+        {
+            return null;
+        }
+
+        GameObject go = new GameObject("SpatialAudio - Temp");
+        go.transform.position = pos;
+
+        AudioSource source = go.AddComponent<AudioSource>();
+        source.clip = clip;
+
+        //source.pitch = randomizePitch;
+        source.spatialBlend = spatialBlend;
+        source.volume = volume;
+        source.Play();
+
+        Destroy(go, clip.length);
+
+        return source;
+    }
+
+}
