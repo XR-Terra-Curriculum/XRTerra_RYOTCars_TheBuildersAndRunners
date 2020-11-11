d17616d6677cf9adeb5c4233f264df0fa1e98327
Liz Newton-Tanzer <54453455+lizecaint@users.noreply.github.com>
2020-11-11 14:44:18 -0500

    Added prototype showroom and rotating stage

---
+++ Assets/SelfRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotate : MonoBehaviour
+{
+    public float rotateSpeed =1f;
+
+    void FixedUpdate()
+    {
+        this.transform.Rotate(0, rotateSpeed * 0.01f, 0, Space.Self);
+    }
+}
