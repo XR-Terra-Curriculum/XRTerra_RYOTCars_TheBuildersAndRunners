865930c6bb91243084b8725b3887680297684b66
Liz Newton-Tanzer <54453455+lizecaint@users.noreply.github.com>
2020-11-11 17:03:03 -0500

    Rotate On Off, Car headlights On Off implemented

--- Assets/SelfRotate.cs
+++ Assets/SelfRotate.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class SelfRotate : MonoBehaviour
 {
     public float rotateSpeed =1f;
+    public bool isRotate = true;
 
     void FixedUpdate()
     {
-        this.transform.Rotate(0, rotateSpeed * 0.01f, 0, Space.Self);
+        if (isRotate)
+        {
+            this.transform.Rotate(0, rotateSpeed * 0.01f, 0, Space.Self);
+        }
+    }
+
+    public void RotateSwitch()
+    {
+        isRotate = !isRotate;
     }
 }
