3b5fa1c3ca89d360379864afa298eee036edf936
Liz Newton-Tanzer <54453455+lizecaint@users.noreply.github.com>
2020-11-14 16:19:57 -0500

    Car Color Select Proptotype Complete

--- Assets/CarSettingsHelper.cs
+++ Assets/CarSettingsHelper.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class CarSettingsHelper : MonoBehaviour
 
 {
     public GameObject CarLights;
+    public Renderer[] carExteriorRenderers;
+    public Renderer[] carInteriorRenderers;
+    public GameObject CarColorChoicesPanel;
+
+ 
 
     void Start()
     {
         CarLights.SetActive(false);
+        CarColorChoicesPanel.SetActive(false);
+
     }
 
     public void SwitchCarLights()
@@ -25,4 +34,45 @@ public class CarSettingsHelper : MonoBehaviour
 
         CarLights.SetActive(!CarLights.activeSelf);
     }
+
+    public void ChangeCarExterior(Material ExteriorMat)
+    {
+        //Console.WriteLine("ChangeCarExterior triggered");
+        print("ChangeCarExterior triggered");
+        foreach (MeshRenderer renderer in carExteriorRenderers)
+        {
+            renderer.material = ExteriorMat;
+            print("renderer.materials[0] = ExteriorMat;");
+        }
+
+        /*
+          Renderer[] children;
+          children = GetComponentsInChildren<Renderer>();
+          foreach (Renderer rend in children)
+          {
+              var mats = new Material[rend.materials.Length];
+              for (var j = 0; j < rend.materials.Length; j++)
+              {
+                  mats[j] = newMat;
+              }
+              rend.materials = mats;
+          }
+              */
+    }
+
+    public void ChangeCarInterior(Material InteriorMat)
+    {
+        foreach(MeshRenderer renderer in carInteriorRenderers)
+        {
+            renderer.material = InteriorMat;
+        }
+    }
+
+
+    public void ShowCarColorChoices(bool show)
+    {
+        CarColorChoicesPanel.SetActive(show);
+    }
+
+
 }
