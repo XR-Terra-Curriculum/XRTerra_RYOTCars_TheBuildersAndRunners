6a979192a4205f0bc02da96f7e9ccc8af6096863
Parmbir Gill <gillp1@wit.edu>
2020-11-18 12:56:22 -0500

    TestDrive
    TestDrive scene in _MyStuff, only implemented offroad and city environment,

---
+++ Assets/_MyStuff/TestDrive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class TestDrive : MonoBehaviour
+{
+    public GameObject car;
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetAxis("XRI_Right_Trigger") != 0)
+        {
+            speed = Input.GetAxis("XRI_Right_Trigger");
+            car.transform.Translate(Vector3.forward * speed);
+        }
+        if (Input.GetAxis("XRI_Left_Trigger") != 0)
+        {
+            speed = Input.GetAxis("XRI_Left_Trigger");
+            car.transform.Translate(Vector3.back * speed);
+        }
+
+    }
+}
