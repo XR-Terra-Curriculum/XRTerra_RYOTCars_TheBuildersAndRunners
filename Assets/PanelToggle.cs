8b0636ee95f5a5852265416081d1f873331dfce8
LegioTitanicus <alexbernstein7170@gmail.com>
2020-11-18 23:46:31 -0500

    Merged environments, car models, hands

---
+++ Assets/PanelToggle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class PanelToggle : MonoBehaviour
+{
+    public GameObject[] panels;
+    public XRController[] controllers;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ShowPanels(panels, controllers);
+    }
+    void ShowPanels(GameObject[] panels, XRController[] controllers)
+    {
+        print("Show panels fired");
+        foreach (XRController controller in controllers)
+        {
+            print("XRController loop running");
+            XRController inputDevice = controller.GetComponent<XRController>();
+            bool inputPrimary = false;
+            bool inputSecondary = false;
+            if (inputDevice.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out inputPrimary) || inputDevice.inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out inputSecondary))
+            {
+                print("XRController input received");
+                foreach (GameObject panel in panels)
+                    if (inputPrimary || inputSecondary)
+                    {
+                        panel.SetActive(!panel.activeSelf);
+                    }
+            }
+        }
+    }
+}
--- Assets/_MyStuff/EnvironmentController.cs
+++ Assets/_MyStuff/EnvironmentController.cs
@@ -12,10 +12,14 @@ public class EnvironmentController : MonoBehaviour
     public Material daySky;
     public Light sunLight;
 
+    public GameObject car;
+    private Transform carPosition;
+
     public void Start()
     {
         RenderSettings.skybox = daySky;
         sunLight.color = Color.white;
+        carPosition = car.GetComponent<Transform>();
     }
     public void Snow()
     {
@@ -23,6 +27,7 @@ public class EnvironmentController : MonoBehaviour
         offRoadTerrain.gameObject.SetActive(false);
         cityTerrain.gameObject.SetActive(false);
         showroomTerrain.gameObject.SetActive(false);
+        ResetCarTransform();
     }
     public void Offroad()
     {
@@ -30,6 +35,7 @@ public class EnvironmentController : MonoBehaviour
         offRoadTerrain.gameObject.SetActive(true);
         cityTerrain.gameObject.SetActive(false);
         showroomTerrain.gameObject.SetActive(false);
+        ResetCarTransform();
     }
     public void City()
     {
@@ -37,6 +43,7 @@ public class EnvironmentController : MonoBehaviour
         offRoadTerrain.gameObject.SetActive(false);
         cityTerrain.gameObject.SetActive(true);
         showroomTerrain.gameObject.SetActive(false);
+        ResetCarTransform();
     }
     public void Showroom()
     {
@@ -44,6 +51,7 @@ public class EnvironmentController : MonoBehaviour
         offRoadTerrain.gameObject.SetActive(false);
         cityTerrain.gameObject.SetActive(false);
         showroomTerrain.gameObject.SetActive(true);
+        ResetCarTransform();
     }
 
     public void Day()
@@ -60,5 +68,13 @@ public class EnvironmentController : MonoBehaviour
         sunLight.color = Color.blue;
         //Change SkyBox to night
         RenderSettings.skybox = nightSky;
+    }
+
+    public void ResetCarTransform()
+    {
+        /*car.transform.position = carPosition.position;
+        car.transform.rotation = carPosition.rotation;*/
+        car.transform.position = new Vector3(2.525f, 173.9451f, 11.34f);
+        car.transform.eulerAngles = new Vector3(0f, 180f, 0f);
     }
 }
